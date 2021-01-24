6c329c7d0cbc959337016b1401e1e72b3a008d0e
CanOklay <canoklay@gmail.com>
2021-01-24 12:20:08 +0300

    Shoppingcart id

--- src/test/java/com/testinium/constants/ConstantsProductPage.java
+++ src/test/java/com/testinium/constants/ConstantsProductPage.java
@@ -5,6 +5,6 @@ import org.openqa.selenium.By;
 public class ConstantsProductPage {
 
     public static final By ADDTOCART = By.id("addToCart");
-    public static final By SHOPPINGCART = By.cssSelector(".sf-SalesFrontCashProductInfo-1SxJq.sf-SalesFrontCashProductInfo-341zE .sf-SalesFrontCashProductInfo-343IQ .sf-SalesFrontCashProductInfo-4KebB:not(:last-child)");
+    public static final By SHOPPINGCART = By.id("shoppingCart");
 
 }
